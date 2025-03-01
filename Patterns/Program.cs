58698c39133135d8a5ba97ea5cb4a9ad7ca8d331
Alex <119462299+RaphaelsHub@users.noreply.github.com>
2025-03-01 09:06:17 -0800

    Created solution.

---
+++ Patterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using Patterns.CreationalPatterns.Singleton;
+
+namespace Patterns;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(ConfigurationManager.Instance.DeviceModel.DeviceName);
+        Console.WriteLine(ConfigurationManager.Instance.DeviceModel.OperatingSystem);
+        Console.WriteLine(ConfigurationManager.Instance.DeviceModel.Storage);
+    }
+}
